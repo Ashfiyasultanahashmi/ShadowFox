ceb6871614e29faeae9a96b3d614a2432d838734
Ashfiyasultanahashmi <144239083+Ashfiyasultanahashmi@users.noreply.github.com>
2025-01-15 09:01:30 +0530

    Internship Task-1

---
+++ Beginner's Level Tasks/Task-1 Numbers/number-03.py
@@ -0,0 +1,5 @@
+#3. If you cross a 490meterlong street in 7 minutes, calculate your speed in meters per second. Print the answer without any decimal point in it.
+distance=490
+time_in_seconds=7*60
+speed=int(distance/time_in_seconds)
+print("The speed in meters per second is:", speed)
