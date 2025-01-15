94bad8ada2e38fd8013bd5ae4e3cd90e2c59f48d
Ashfiyasultanahashmi <144239083+Ashfiyasultanahashmi@users.noreply.github.com>
2025-01-15 08:45:32 +0530

    Internship Task-1

---
+++ Beginner's Level Tasks/Task-1 Variables/variable-03.py
@@ -0,0 +1,6 @@
+#3. Store the principal amount, rate of interest, and time indifferent variables and then calculate the Simple Interest for 3 years. Formula: Simple Interest = P x R x T / 100
+Principal_amount=int(input("Enter principal amount:"))
+Rate= int(input("Enter rate_of_interest:")) 
+Time=3
+SI=(Principal_amount*Rate*Time)/100
+print(f"The Simple Interest (SI) is: {SI}")
