0ac7166d4323ca9a70d2a18b96d17664aa597d83
Ashfiyasultanahashmi <144239083+Ashfiyasultanahashmi@users.noreply.github.com>
2025-01-15 08:43:51 +0530

    Internship Task-1

---
+++ Beginner's Level Tasks/Task-1 Variables/variable-02.py
@@ -0,0 +1,4 @@
+#2. Create a variable called for and assign it a value 4. See whathappens and find out the reason behind the behavior that you see.
+for = 4
+#The word "for" is a reserved keyword in python, which cannot be used as a name for any variables. Hence no output is produced and an error is generated, i.e., SyntaxError: invalid syntax
+print("The variable name "for" is invalid, and hence a Syntax error is obtained")
