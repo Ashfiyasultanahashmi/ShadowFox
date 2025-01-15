71d954bf8e5395b949d47f93ad4f86131b467003
Ashfiyasultanahashmi <144239083+Ashfiyasultanahashmi@users.noreply.github.com>
2025-01-15 08:39:54 +0530

    Internship Task-1

--- Internship/Beginner Level Tasks/Task-1 Variables/variable_01.py
+++
@@ -1,4 +0,0 @@
-#1. Create a variable named pi and store the value 22/7 in it. Now check the data type of this variable.
-pi = 22/7
-print("The value of pi is=", pi)
-print("The data type of pi is:", type(pi)
